b43807d1c19d2a1f5c4a6d97494df0c09c266c8c
Bulochk1n <160772938+Bulochk1n@users.noreply.github.com>
2026-02-11 21:19:42 +0100

    Update API_URL to use VITE_API_URL environment variable

--- xyvox-frontend/src/api/axios.ts
+++ xyvox-frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = import.meta.env.PROD ? '/api' : 'http://localhost:8080/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
 
 export const api = axios.create({
     baseURL: API_URL,
